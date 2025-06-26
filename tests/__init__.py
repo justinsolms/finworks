a7ba7a163136d027fb02990e011e28cebf403bbb
justinsolms <justinsolms@gmail.com>
2025-06-26 10:55:18 +0200

    A few files were missing orm this branch and there was a `src/fundmanage` folder with files in it moved to the `src/finworks` folder. The MANIFEST.in and .toml files are still missing so FF-merge into this main branch when all split work is completed and released.

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+"""Testing fundmanage package.
+
+Copyright (C) 2015 Justin Solms <justinsolms@gmail.com>.
+This file is part of the fundmanage module.
+The fundmanage module can not be modified, copied and/or
+distributed without the express permission of Justin Solms.
+
+"""
